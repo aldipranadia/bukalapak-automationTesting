2d3b3362e6668c6b13fc8ff8a95a194e95e8d0be
aldipranadia <aldipranadiaa@gmail.com>
2022-04-13 14:26:52 +0700

    first commit

---
+++ Test Suites/Bukalapak/Regression Test.ts
@@ -0,0 +1,44 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Regression Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>abc61905-d0cf-43d2-be79-cc9a0a472b05</testSuiteGuid>
+   <testCaseLink>
+      <guid>a8abf568-4528-445b-90af-3c2960693cf4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Bukalapak/Login_Valid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>898ddc98-5a6a-4755-8ea6-77eac65a2671</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Bukalapak/Login_Email_Invalid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b96a8516-fe24-401f-9f9d-fad915e3f8fb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Bukalapak/Login_Email_Null</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e4dc1959-3339-4ef9-ad8f-679f40242cd6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Bukalapak/Login_Password_Invalid</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7782f167-25c9-44be-a418-1de4907722b4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Bukalapak/Login_Password_Null</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
